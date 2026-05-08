c33f558974f3f5460030e4b62fc02abd27dbc65e
aozbas <alpaslan.ozbas@gmail.com>
2026-05-07 23:05:07 -0700

    backend: rename HOSTED_DATABASE_KEY to HOSTED_DATABASE_SERVICE_ROLE_KEY and fix MODEL_PATH

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,12 +4,12 @@ from hosted_database import create_client, Client
 
 class Settings(BaseSettings):
     hosted_database_url: str = ""
-    hosted_database_key: str = ""
-    model_path: str = "ml/models/lesion_classifier_binary.pt"
+    hosted_database_service_role_key: str = ""
+    model_path: str = "../ml/models/lesion_classifier_binary.pt"
 
     class Config:
         env_file = ".env"
 
 
 settings = Settings()
-hosted_database: Client = create_client(settings.hosted_database_url, settings.hosted_database_key)
+hosted_database: Client = create_client(settings.hosted_database_url, settings.hosted_database_service_role_key)
